6725a70f70eaa21df155dd749ecbd599c7b61c22
Fr Rj <forneriarj@gmail.com>
2025-10-11 02:03:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4845954183-96505",
+  "appId": "1:496530050961:web:f19786e63afb79ee145430",
+  "apiKey": "AIzaSyDf156bePnHQThx507bxjGampbCH4vb2Iw",
+  "authDomain": "studio-4845954183-96505.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "496530050961"
+};
