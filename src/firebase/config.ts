48ce48eac40dd1ac2838abbd75937722f5e22614
Fr Rj <forneriarj@gmail.com>
2025-10-14 03:28:48 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,6 @@ export const firebaseConfig = {
   "appId": "1:496530050961:web:f19786e63afb79ee145430",
   "apiKey": "AIzaSyDf156bePnHQThx507bxjGampbCH4vb2Iw",
   "authDomain": "studio-4845954183-96505.firebaseapp.com",
-  "storageBucket": "studio-4845954183-96505.appspot.com",
   "measurementId": "",
   "messagingSenderId": "496530050961"
 };
